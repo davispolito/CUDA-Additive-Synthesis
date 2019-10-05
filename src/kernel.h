e6e32282a0b568ed8aeb3554b3a6e2d006b602ff
Davis Polito <dpolito@sas.upenn.edu>
2019-10-04 20:14:57 -0400

    In order to make a pie from scratch you must first recreate the universe

---
+++ src/kernel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <chrono>
+#include <stdexcept>
